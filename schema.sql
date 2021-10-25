e7e021519a2107e5b47ccb6d26b969c526801ddc
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-25 15:40:41 +0100

    Change VARCHAR Value In schema.sql File

--- schema.sql
+++ schema.sql
@@ -2,7 +2,7 @@
 
 CREATE TABLE animals (
  id INT GENERATED ALWAYS AS IDENTITY,
- name VARCHAR(100),
+ name VARCHAR(50),
  date_of_birth DATE,
  escape_attempts INT,
  neutered BOOLEAN DEFAULT false,
