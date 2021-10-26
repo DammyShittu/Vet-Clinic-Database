7dbf60eabe5471e06d1207eb8620674f9cdeaa0d
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-26 12:32:38 +0100

    Add New Column To Animals Table

--- schema.sql
+++ schema.sql
@@ -1,5 +1,4 @@
 /* Database schema to keep the structure of entire database. */
-
 CREATE TABLE animals (
  id INT GENERATED ALWAYS AS IDENTITY,
  name VARCHAR(50),
@@ -9,3 +8,6 @@ CREATE TABLE animals (
  weight_kg DECIMAL,
  PRIMARY KEY(id)
  );
+
+-- Add a column species of type string to your animals table.
+ ALTER TABLE animals ADD species VARCHAR(50);
