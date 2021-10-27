c013b367b4c69f3c38f0df56123cbb8ce1db8683
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-27 14:00:25 +0100

    Create New Tables And Alter Animals Table

--- schema.sql
+++ schema.sql
@@ -7,7 +7,26 @@ CREATE TABLE animals (
  neutered BOOLEAN DEFAULT false,
  weight_kg DECIMAL,
  PRIMARY KEY(id)
- );
+);
 
 -- Add a column species of type string to your animals table.
  ALTER TABLE animals ADD species VARCHAR(50);
+
+
+CREATE TABLE owners(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ full_name VARCHAR(50),
+ age INT
+);
+
+CREATE TABLE species(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(50)
+);
+
+ALTER TABLE animals DROP COLUMN species;
+
+ALTER TABLE animals ADD species_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
+ALTER TABLE animals ADD owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
