1450413bea6daf52b9aeedc0783dba1848e927ee
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-28 16:23:41 +0100

    Create Join Tables For Many To Many Relationships

--- schema.sql
+++ schema.sql
@@ -33,3 +33,35 @@ ALTER TABLE animals ADD species_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_name FOREIGN KEY(species_id) REFERENCES species(id);
 ALTER TABLE animals ADD owner_id INT;
 ALTER TABLE animals ADD CONSTRAINT fk_name2 FOREIGN KEY(owner_id) REFERENCES owners(id);
+
+
+-- Day 4
+
+-- Create Vets Table
+CREATE TABLE vets(
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ name VARCHAR(50),
+ age INT,
+ date_of_graduation DATE
+);
+
+-- Create Specializations Table
+CREATE TABLE specializations (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ vets_id INT,
+ species_id INT,
+ CONSTRAINT vet_fk FOREIGN KEY(vets_id) REFERENCES vets(id),
+ CONSTRAINT species_fk FOREIGN KEY(species_id) REFERENCES species(id)
+);
+
+--  Create Visits Table
+CREATE TABLE visits (
+ id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+ animals_id INT,
+ vets_id INT,
+ date_of_visit DATE,
+ CONSTRAINT vet_fk FOREIGN KEY(vets_id) REFERENCES vets(id),
+ CONSTRAINT animals_fk FOREIGN KEY(animals_id) REFERENCES animals(id), 
+);
+
+
